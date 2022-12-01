14c4cd559142d0ea746dae38c3266b43bd1c20a2
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-12-01 18:40:16 -0500

    massive location

--- src/routes/App.routing.tsx
+++ src/routes/App.routing.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from '../modules/Authentication/Login/Login';
 import Home from '../pages/Home';
+import InsertMassive from '../pages/InsertMassive';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route index element={<Login />} />
             <Route path="home" element={<Home />} />
+            <Route path="inserts" element={<InsertMassive />} />
         </Routes>
     );
 }
